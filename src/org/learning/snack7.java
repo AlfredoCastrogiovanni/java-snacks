78b3f0cd3b3eee6774d4d54d8a02a7447d7c4ead
AlfredoCastrogiovanni <castrogiovanni.alfredo03@gmail.com>
2024-05-16 14:53:51 +0200

    snack7 done

---
+++ src/org/learning/snack7.java
@@ -0,0 +1,22 @@
+package org.learning;
+
+import java.util.Scanner;
+
+public class snack7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the seconds: ");
+        int seconds = Integer.parseInt(scanner.nextLine());
+
+        int hours = seconds / 3600;
+
+        int minutes = (seconds % 3600) / 60;
+
+        int remainingSeconds = seconds % 60;
+
+        System.out.println(hours + ":" + minutes + ":" + remainingSeconds);
+
+        scanner.close();
+    }
+}
